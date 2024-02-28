ec235fb9abd68c307243a11e01b58f24a4b63498
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-28 13:37:10 +0600

    доделал тест

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,13 @@
 import Cart from '../service/Cart';
+import Book from '../domain/Book';
 
 test('new card should be empty', () => {
   const cart = new Cart();
-
   expect(cart.items.length).toBe(0);
+});
+
+test('add in cart', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225));
+  expect(cart.items.length).toBe(1);
 });
