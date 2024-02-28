e83add0ccda8e0b3275b2a3d60f4e7512d164e4c
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-28 13:07:26 +0600

    Обновил некоторые файлы (web, tsconf, jest.conf), добавил app and Movie

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,25 @@
-import Cart from './service/Cart';
-import Book from './domain/Book';
-import MusicAlbum from './domain/MusicAlbum';
+import Cart from "./service/Cart";
+import Book from "./domain/Book";
+import MusicAlbum from "./domain/MusicAlbum";
+import Movie from "./domain/Movie";
 
 const cart = new Cart();
 console.log(cart.items);
 
-cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
-cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+cart.add(new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225));
+cart.add(new MusicAlbum(1008, "Meteora", "Linkin Park", 900));
 
+console.log(cart.items);
+
+cart.add(new Movie(
+  23,
+  "Мстители",
+  "The Avengers",
+  2012,
+  "США",
+  "Avengers Assemble!",
+  ["фантастика", "боевик", "фентези", "приключения"],
+  137,
+  100
+));
 console.log(cart.items);
